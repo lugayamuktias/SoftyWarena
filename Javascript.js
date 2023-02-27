de12802eed85bcbe4e152bd74177d88d7afb99b9
Lugaya Mukti <114932064+lugayamuktias@users.noreply.github.com>
2023-02-27 16:02:19 +0700

    Add files via upload
    Update Website

---
+++ Javascript.js
@@ -0,0 +1,7 @@
+const body = document.body;
+
+const Motto = document.createElement("p");
+Motto.innerHTML =
+  "<marquee>Selamat Datang di Website SOFTY WARENA. Website ini dibangun dan dikelola oleh Lugaya Mukti Arwasaputra</marquee>";
+
+body.append(Motto);
